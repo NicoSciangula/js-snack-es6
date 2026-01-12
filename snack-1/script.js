b8f27e81f48a1bc1a2da91d25d2f9f3ded6d30ee
NicoSciangula <nico.sciangula99@gmail.com>
2026-01-12 19:59:35 +0100

    created variables

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,27 @@
 // * Creare un array di oggetti:
 // *Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // *Stampare a schermo la bici con peso minore.
+// ! dichiarazione variabili
+const listaBici = [
+  { nome: "Aquila RS", peso: 6.9 },
 
+  { nome: "Vento Veloce", peso: 7.2 },
+
+  { nome: "Fulmine Pro", peso: 6.7 },
+
+  { nome: "Strada Nero", peso: 7.6 },
+
+  { nome: "Tempesta Carbon", peso: 7.0 },
+
+  { nome: "Sprinta X", peso: 7.4 },
+
+  { nome: "Falco Aereo", peso: 7.8 },
+
+  { nome: "Corsa Titan", peso: 8.1 },
+
+  { nome: "Velocità One", peso: 6.8 },
+
+  { nome: "Eclisse Racing", peso: 7.1 },
+];
+
+const text = document.getElementById("text-display");
