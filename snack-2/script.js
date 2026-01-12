96073ec6ea16e2016b1174444a7585bba734c87e
NicoSciangula <nico.sciangula99@gmail.com>
2026-01-12 21:50:43 +0100

    variable declaration

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,3 +4,10 @@
 //* Generare numeri random al posto degli 0 nelle proprietà “punti fatti” e “falli subiti”.
 //* Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
+const squadre = [
+  { nome: "Juventus", puntiFatti: 0, falliFatti: 0 },
+  { nome: "Milan", puntiFatti: 0, falliFatti: 0 },
+  { nome: "Inter", puntiFatti: 0, falliFatti: 0 },
+  { nome: "Roma", puntiFatti: 0, falliFatti: 0 },
+  { nome: "Torino", puntiFatti: 0, falliFatti: 0 },
+];
